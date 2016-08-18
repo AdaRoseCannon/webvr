4da90dc4623c002d0db099c06dbd2cf53457d70d
Ada Rose Edwards <ada@ada.is>
2016-08-18 11:28:26 +0100

    Use superstandard for water

--- a-frame-assets/ada-components/super-standard-material.js
+++ a-frame-assets/ada-components/super-standard-material.js
@@ -21,17 +21,16 @@ function updateNormal(shader, data) {
 		if (data.normalTextureWrap === 'repeat') {
 			texture.wrapS = texture.wrapT = THREE.RepeatWrapping
 		}
-		// if (data.normalTextureWrap === 'clamp') {
-		// 	texture.wrapS = texture.wrapT = THREE.ClampToEdgeWrapping
-		// }
-		// if (data.normalTextureWrap === 'mirrored') {
-		// 	texture.wrapS = texture.wrapT = THREE.MirroredRepeatWrapping
-		// }
-		// if (data.normalTextureOffset) {
-		// 	texture.offset = data.normalTextureOffset;
-		// }
+		if (data.normalTextureWrap === 'clamp') {
+			texture.wrapS = texture.wrapT = THREE.ClampToEdgeWrapping
+		}
+		if (data.normalTextureWrap === 'mirrored') {
+			texture.wrapS = texture.wrapT = THREE.MirroredRepeatWrapping
+		}
+		if (data.normalTextureOffset) {
+			texture.offset = data.normalTextureOffset;
+		}
 		if (data.normalTextureRepeat) {
-			console.log(data.normalTextureRepeat);
 			texture.repeat = (new THREE.Vector2()).copy(data.normalTextureRepeat);
 		}
 
@@ -161,3 +160,35 @@ function getMaterialData (data) {
 		roughness: data.roughness
 	};
 }
+
+AFRAME.registerComponent('wobble-normal', {
+	schema: {},
+	tick: function (t) {
+		this.el.components.material.material.normalMap.offset.x += 0.0001 * Math.sin(t/10000);
+		this.el.components.material.material.normalMap.offset.y += 0.0001 * Math.cos(t/8000);
+		this.el.components.material.material.normalScale.x = 0.5 + 0.5 * Math.cos(t/1000);
+		this.el.components.material.material.normalScale.x = 0.5 + 0.5 * Math.sin(t/1200);
+	}
+})
+
+AFRAME.registerPrimitive('a-ocean-plane', {
+	defaultComponents: {
+		geometry: {
+			primitive: 'plane',
+			height: 10000,
+			width: 10000
+		},
+		rotation: '-90 0 0',
+		material: {
+			shader: 'super-standard',
+			color: '#8ab39f',
+			metalness: 1,
+			roughness: 0.2,
+			normalTextureRepeat: '50 50',
+			normalTextureOffset: '0 0',
+			normalScale: '0.5 0.5',
+			opacity: 0.8
+		},
+		'wobble-normal': {}
+	},
+});
